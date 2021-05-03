8a9a4e92c933c5b367ccb37bbdef506ba32b865b
daiki510 <ddgg7755@gmail.com>
2021-05-03 22:21:38 +0900

    テーブル作成

---
+++ backend/database/migrations/2021_05_03_130439_create_chapters_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChaptersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chapters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('comic_id')->unsigned()->index();
+            $table->integer('chapter_no');
+            $table->text('chapter_url');
+            $table->string('chapter_title');
+            $table->timestamps();
+
+            $table->foreign('comic_id')
+                    ->references('id')
+                    ->on('comics')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chapters');
+    }
+}
